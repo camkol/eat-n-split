c0ca44f1189da25c2392c51346657a287e36fd05
camkol <koliyus3@hotmail.com>
2024-05-13 17:52:44 +0200

    more controls and readme

--- src/App.js
+++ src/App.js
@@ -153,20 +153,40 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <form className="form-split-bill">
       <h2>Split bill with {selectedFriend.name} </h2>
       <label>💲 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>🧍‍♂️ Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? bill : Number(e.target.value)
+          )
+        }
+      />
 
       <label>🧍‍♀️ {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤷‍♂️ Who is paying the bill </label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">Me</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
