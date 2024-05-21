814fcba314a3b46433eb8701887dd7c17988cf52
camkol <koliyus3@hotmail.com>
2024-05-21 06:17:02 +0200

    added react?

--- src/FormSplitBill.js
+++ src/FormSplitBill.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 export default function FormSplitBill({ selectedFriend, onSplitBill }) {
